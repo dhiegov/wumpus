d627dbd5323b1274da66d92767a4725973c73f9d
AquaMouzinho <42475699+AquaMouzinho@users.noreply.github.com>
2025-08-06 14:15:01 -0300

    Update ambiente.py

---
+++ ambiente.py
@@ -0,0 +1,11 @@
+class Ambiente: 
+  # Inicialização do ambiente
+  def gerar_grid_aleatorio(self, n, m):
+     return 0
+  
+  def __init__(self, n = 10, m = 10):
+      
+      self.grid = gerar_grid_aleatorio(n,m)
+      self.agente_pos = (0,0)
+
+  
